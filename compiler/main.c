e95cf6bd845649afcd298761bdc0e40251ec8213
TropicSapling <tropical.productions.scratch@gmail.com>
2017-07-27 19:17:34 +0200

    Basic I/O

--- compiler/main.c
+++ compiler/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <errno.h>
 
 void println(char str[]) {
 	printf("%s\n", str);
@@ -12,7 +13,21 @@ int main(int argc, char *argv[]) {
 	
 	println("Compiling...");
 	
-//	FILE *file = fopen(argv[1], "r"); // Will be "r+" if automatic compiled GC gets added in the future
+	FILE *file = fopen(argv[1], "r"); // Will be "r+" if automatic compiled GC gets added in the future
+	
+	if(file == NULL) {
+		perror("ERROR");
+		fprintf(stderr, "ID: %d\n", errno);
+		return 1;
+	}
+	
+	char buf[255];
+	
+	while(fgets(buf, 255, file) != NULL) {
+		println(buf);
+	}
+	
+	fclose(file);
 	
 	return 0;
 }
