4edcfd9940a994e14677996a1a6190bc5149eccf
TropicSapling <tropical.productions.scratch@gmail.com>
2017-07-31 23:20:34 +0200

    Start of lexer

--- compiler/main.c
+++ compiler/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -24,11 +25,11 @@ int main(int argc, char *argv[]) {
 	FILE *output;
 	
 	if(argc < 3) {
-		unsigned char file_length = strlen(argv[1]);
+		size_t file_length = strlen(argv[1]);
 		char filename[file_length];
 		strcpy(filename, argv[1]);
 		
-		unsigned char c = file_length - 1;
+		size_t c = file_length - 1;
 		while(filename[c] != '.') {
 			c--;
 		}
@@ -69,19 +70,48 @@ int main(int argc, char *argv[]) {
 	
 	fprintf(output, "#include <stdio.h>\nint main(int argc, char *argv[]) {\n");
 	
-	char buf[128];
-	float i = 0.0;
+	fseek(input, 0L, SEEK_END);
+	size_t file_size = ftell(input);
+	rewind(input);
 	
-	while(fgets(buf, 128, input) != NULL) {
-		char *cout_pos = strstr(buf, "cout");
+	char buf[65536];
+	double d = 0.0;
+	
+	while(fgets(buf, 65536, input) != NULL) {
+		char *c = buf;
+		char *keywords[] = {c};
+		size_t row_len = 0;
+		size_t keywords_size = 1;
 		
-		if(cout_pos != NULL) {
-			fprintf(output, "printf(\"%%s\", %s);\n", cout_pos + 5);
+		while(*c != '\0') {
+			while(*c != ' ' && *c != '\0') {
+				c++;
+				row_len++;
+			}
+			
+			*c = '\0'; // use memset if this doesn't work
+			
+			c++;
+			row_len++;
+			keywords_size++;
+			
+			char *res = realloc(keywords, keywords_size);
+			if(res == NULL) {
+				perror("ERROR");
+				fprintf(stderr, "ID: %d\n", errno);
+				return 1;
+			}
+			
+			keywords[keywords_size - 1] = c;
 		}
 		
-		printf("Compiling... %.2f%%\r", (i / 500) * 100); // Made up progress for now
+/*		for(int i = 0; i < strlen(keywords); i++) {
+			fprintf(output, "%s", *keywords[i]);
+		} */
+		
+		d += row_len;
+		printf("Compiling... %.2f%%\r", (d / file_size) * 100);
 		fflush(stdout);
-		i++;
 	}
 	
 	fclose(input);
