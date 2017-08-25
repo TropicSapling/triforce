c8825c69be3f10604445a447128dc9a9313e87d0
TropicSapling <tropical.productions.scratch@gmail.com>
2017-08-25 20:48:38 +0200

    Preparement for parsing

--- compiler/src/parser.c
+++ compiler/src/parser.c
@@ -18,25 +18,26 @@ char *addSpaceForChars(char **keywords, size_t *keywords_size) {
 	return res;
 }
 
-char *parse(char **keywords, size_t key, size_t *pos) {
+char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
+	char ****types = {
+		{{"array", "bool", "chan", "func", "list", "pointer", "var", "void"}, {}},
+		{{"char", "int", "number"}, {"array", "list", "pointer"}},
+		{{"const", "only"}, {"array", "char", "decimal", "int", "list", "number", "pointer", "signed", "string", "unsigned", "var"}},
+		{{"decimal"}, {"number"}},
+		{{"noscope"}, {"array", "char", "const", "decimal", "int", "list", "number", "only", "pointer", "signed", "string", "unsigned", "var"}},
+		{{"signed", "unsigned"}, {"char", "int", "number", "string"}}
+	};
+	
 	size_t output_size = 256;
 	char *output = malloc(output_size);
 	
 	for(size_t i = 0; i < key; i++) {
 		if(keywords[i] != NULL) {
-			// DEBUG; will be replaced later
-			for(int it = 0; keywords[i][it] != '\0'; it++) {
-				if(*pos >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
-					return NULL;
-				}
-				
-				output[*pos] = keywords[i][it];
-				(*pos)++;
-			}
-			
-			if(*pos >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
+			if(*pos + 1 >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
 				return NULL;
 			}
+			
+			// WIP
 			
 			output[*pos] = ' ';
 			(*pos)++;
