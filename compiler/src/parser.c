4ea51c3850b0ddcad19c7b805c08c1018359438e
TropicSapling <tropical.productions.scratch@gmail.com>
2017-08-26 18:14:53 +0200

    Added 'findItems' function to 'std' library
    Also fixed some bugs

--- compiler/src/parser.c
+++ compiler/src/parser.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <string.h>
 
 #include "def.h"
 
@@ -19,28 +20,33 @@ char *addSpaceForChars(char **keywords, size_t *keywords_size) {
 }
 
 char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
-	char ****types = {
+/*	char types[][2][12][8] = {
 		{{"array", "bool", "chan", "func", "list", "pointer", "var", "void"}, {}},
 		{{"char", "int", "number"}, {"array", "list", "pointer"}},
-		{{"const", "only"}, {"array", "char", "decimal", "int", "list", "number", "pointer", "signed", "string", "unsigned", "var"}},
-		{{"decimal"}, {"number"}},
-		{{"noscope"}, {"array", "char", "const", "decimal", "int", "list", "number", "only", "pointer", "signed", "string", "unsigned", "var"}},
-		{{"signed", "unsigned"}, {"char", "int", "number", "string"}}
-	};
+		{{"const", "only"}, {"array", "char", "fraction", "int", "list", "number", "pointer", "signed", "unsigned", "var"}},
+		{{"fraction"}, {"number"}},
+		{{"noscope"}, {"array", "char", "const", "fraction", "int", "list", "number", "only", "pointer", "signed", "unsigned", "var"}},
+		{{"signed", "unsigned"}, {"char", "int", "number"}}
+	}; */
 	
 	size_t output_size = 256;
 	char *output = malloc(output_size);
 	
 	for(size_t i = 0; i < key; i++) {
 		if(keywords[i] != NULL) {
-			if(*pos + 1 >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
-				return NULL;
+			if(strcmp(keywords[i], "unsigned") == 0 || strcmp(keywords[i], "signed") == 0 || strcmp(keywords[i], "int") == 0 || strcmp(keywords[i], "char") == 0 || strcmp(keywords[i], "const") == 0 || strcmp(keywords[i], "void") == 0) {
+				if(*pos + 1 >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
+					return NULL;
+				}
+				
+				for(unsigned int j = 0; j < strlen(keywords[i]); j++) {
+					output[*pos] = keywords[i][j];
+					(*pos)++;
+				}
+				
+				output[*pos] = ' ';
+				(*pos)++;
 			}
-			
-			// WIP
-			
-			output[*pos] = ' ';
-			(*pos)++;
 		}
 	}
 	
