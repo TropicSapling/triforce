1bd537e2ef3a0e05ed22fd3f4d23eb90b89c05be
TropicSapling <tropical.productions.scratch@gmail.com>
2017-09-10 18:15:11 +0200

    Started working on '>>>' and '<<<' operators

--- compiler/src/parser.c
+++ compiler/src/parser.c
@@ -34,6 +34,13 @@ bool arrContains(char arr[][8], char *str, unsigned int size){
 	return false;
 }
 
+void typeTo(char *output, char *str, size_t *pos) {
+	for(unsigned int i = 0; str[i] != '\0'; i++) {
+		output[*pos] = str[i];
+		(*pos)++;
+	}
+}
+
 char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
 	char types[22][8] = {"bool", "chan", "char", "clang", "const", "fraction", "func", "heap", "int", "list", "noscope", "number", "only", "pointer", "register", "signed", "stack", "static", "unique", "unsigned", "void", "volatile"};
 	char reserved_keys[19][8] = {"async", "break", "case", "continue", "default", "do", "else", "eval", "export", "foreach", "goto", "if", "import", "in", "repeat", "return", "switch", "type", "while"};
@@ -43,7 +50,7 @@ char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
 	
 	for(size_t i = 0; i < key; i++) {
 		if(keywords[i] != NULL) {
-			if(strcmp(keywords[i], "@") == 0) {
+			if(keywords[i][0] == specials[22]) {
 				// POINTER ACCESS
 				
 				if(*pos >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
@@ -126,6 +133,59 @@ char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
 				puts("----------------------------------------------------------------");
 				printf(YELLOW "[WARNING]" RESET " 'clang' is not implemented yet.\n"); // WIP
 				puts("----------------------------------------------------------------");
+			} else if(keywords[i][0] == specials[2]) {
+				// LISTS
+				
+				if(strcmp(keywords[i + 1], "when") == 0) {
+					// WIP
+				} else {
+/*					for(unsigned int j = 1; keywords[i + j] != specials[3]; j++) {
+						if(keywords[i + j] != '>' || keywords[i + j + 1] != '>' || keywords[i + j + 2] != '>') {
+							typeTo(output, "for(unsigned int ", pos);
+							typeTo(output, ITERATOR_NAME, pos); // REPLACE ITERATOR_NAME
+							output[*pos] = '=';
+							(*pos)++;
+							
+							unsigned int k = 1;
+							for(; k < j; k++) {
+								output[*pos] = keywords[i + k];
+								(*pos)++;
+							}
+							
+							if(k == 1) {
+								output[*pos] = '0';
+								(*pos)++;
+							}
+							
+							output[*pos] = ';';
+							(*pos)++;
+							
+							typeTo(output, ITERATOR_NAME, pos);
+							output[*pos] = '<';
+							(*pos)++;
+							
+							unsigned int l = k + 3;
+							for(; keywords[l] != specials[3]; l++) {
+								output[*pos] = keywords[i + l];
+								(*pos)++;
+							}
+							
+							if(l == 1) {
+								typeTo(output, NAME_SIZE, pos); // REPLACE NAME_SIZE
+							}
+							
+							output[*pos] = ';';
+							(*pos)++;
+							
+							typeTo(output, ITERATOR_NAME, pos);
+							typeTo(output, "++", pos);
+							
+							break;
+						} else if(keywords[i] != '<' || keywords[i + j + 1] != '<' || keywords[i + j + 2] != '<') {
+							break;
+						}
+					} */
+				}
 			} else if(!arrContains(types, keywords[i], 22) && !arrContains(reserved_keys, keywords[i], 19) && strstr(specials, keywords[i]) == NULL) {
 				for(int it = 0; keywords[i][it] != '\0'; it++) {
 					if(*pos >= output_size && addSpaceForChars(&output, &output_size) == NULL) {
@@ -136,14 +196,7 @@ char *parse(char **keywords, size_t key, size_t *pos, char specials[]) {
 					(*pos)++;
 				}
 				
-				output[*pos] = '_';
-				(*pos)++;
-				output[*pos] = 'p';
-				(*pos)++;
-				output[*pos] = 'p';
-				(*pos)++;
-				output[*pos] = 'l';
-				(*pos)++;
+				typeTo(output, "_ppl", pos);
 			} else {
 				// DEBUG; will be replaced later
 				for(int it = 0; keywords[i][it] != '\0'; it++) {
