919f521feb606bb966bcc925e192cd0ed5d79504
TropicSapling <tropical.productions.scratch@gmail.com>
2017-10-13 22:41:10 +0200

    Better error reporting

--- compiler/src/def.h
+++ compiler/src/def.h
@@ -10,10 +10,32 @@
 	extern size_t pkey;
 
 	extern size_t pos;
+	
+	struct ErrInfo {
+		char pointer msg;
+		
+		char pointer filename;
+		size_t lineno;
+		size_t linecol;
+		
+		char pointer data;
+		char pointer*2 data2;
+		unsigned int i;
+	};
+	
+	void printErr(struct ErrInfo Error);
+	void printWarning(struct ErrInfo Error);
 
 	void preprocess(FILE pointer*2 input, char pointer*2 processed_input, size_t input_size, char pointer path[static 2], char pointer*2 exports, size_t pointer exports_size, size_t pointer ekey, char defs[128][2][128], size_t pointer defID);
 
 	void lex_parse(char pointer input, char pointer*3 keywords, char pointer*3 pointers);
 
 	char pointer parse(char pointer*2 keywords, char pointer filename);
+	
+	#define RED   "\x1B[31m"
+	#define GREEN   "\x1B[32m"
+	#define YELLOW   "\x1B[33m"
+	#define BLUE   "\x1B[34m"
+	#define WHITE   "\x1B[37;1m"
+	#define RESET "\x1B[0m"
 #endif
