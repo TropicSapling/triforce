3c1390d617fc39c50b321a5f7cf3917e2eb2be6c
TropicSapling <tropical.productions.scratch@gmail.com>
2017-11-01 19:35:44 +0100

    Bugfixes & improvements

--- compiler/src/def.h
+++ compiler/src/def.h
@@ -29,11 +29,11 @@
 	void printErr(struct ErrInfo Error, unsigned short errType);
 	void safe_realloc(void pointer*2 ptr, size_t size);
 
-	void preprocess(FILE pointer input, char pointer*2 processed_input, size_t pointer input_size, size_t pointer input_item, char pointer path[static 2], char defs[256][2][256], size_t pointer defID);
+	size_t preprocess(FILE pointer input, char pointer*2 processed_input, size_t pointer input_size, size_t pointer input_item, char pointer path[static 2], char defs[256][2][256], size_t pointer defID);
 
 	void lex_parse(char pointer input, char pointer*3 keywords, char pointer*3 pointers);
 	
-	char pointer parse(char pointer*2 keywords, char pointer filename);
+	char pointer parse(char pointer*2 keywords, char pointer filename, size_t extra_lines);
 	
 	#define RED   "\x1B[31m"
 	#define GREEN   "\x1B[32m"
