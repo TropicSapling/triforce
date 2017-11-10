debcd8a2c2a4286d3a7492c841fc6cfe7a369b67
TropicSapling <tropical.productions.scratch@gmail.com>
2017-11-10 21:38:53 +0100

    Second rewrite [WIP, UNSTABLE]

--- compiler/build.sh
+++ compiler/build.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-./old/bin/ppc.out src/main.ppl bin/main.c
-./old/bin/ppc.out src/preprocessor.ppl bin/preprocessor.c
-./old/bin/ppc.out src/lexer.ppl bin/lexer.c
-./old/bin/ppc.out src/parser.ppl bin/parser.c
-./old/bin/ppc.out src/def.h bin/def.h
+./old/alpha-03/bin/ppc.out src/main.ppl bin/main.c
+./old/alpha-03/bin/ppc.out src/preprocessor.ppl bin/preprocessor.c
+./old/alpha-03/bin/ppc.out src/lexer.ppl bin/lexer.c
+./old/alpha-03/bin/ppc.out src/parser.ppl bin/parser.c
 
 gcc -std=c11 bin/main.c bin/preprocessor.c bin/lexer.c bin/parser.c -o bin/ppc.out
 
@@ -14,4 +13,3 @@ rm bin/main.c
 rm bin/preprocessor.c
 rm bin/lexer.c
 rm bin/parser.c
-rm bin/def.h
