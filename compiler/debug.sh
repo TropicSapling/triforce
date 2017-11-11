8a26708c1d25bec850e3dc3458b7584132826e9b
TropicSapling <tropical.productions.scratch@gmail.com>
2017-11-11 19:24:17 +0100

    Bugfixes

--- compiler/debug.sh
+++ compiler/debug.sh
@@ -2,14 +2,8 @@
 
 set -e
 
-./old/alpha-03/bin/ppc.out src/main.ppl bin/main.c -d
-./old/alpha-03/bin/ppc.out src/preprocessor.ppl bin/preprocessor.c -d
-./old/alpha-03/bin/ppc.out src/lexer.ppl bin/lexer.c -d
-./old/alpha-03/bin/ppc.out src/parser.ppl bin/parser.c -d
+./old/alpha-03/bin/ppc.out src/main.ppl bin/main.c
 
-gcc -std=c11 -g bin/main.c bin/preprocessor.c bin/lexer.c bin/parser.c -o bin/ppc.out
+gcc -std=c11 -g bin/main.c -o bin/ppc.out
 
 rm bin/main.c
-rm bin/preprocessor.c
-rm bin/lexer.c
-rm bin/parser.c
--- compiler/install.sh
+++ compiler/install.sh
@@ -3,13 +3,7 @@
 set -e
 
 ./old/alpha-03/bin/ppc.out src/main.ppl bin/main.c
-./old/alpha-03/bin/ppc.out src/preprocessor.ppl bin/preprocessor.c
-./old/alpha-03/bin/ppc.out src/lexer.ppl bin/lexer.c
-./old/alpha-03/bin/ppc.out src/parser.ppl bin/parser.c
 
-gcc -std=c11 -O3 bin/main.c bin/preprocessor.c bin/lexer.c bin/parser.c -o bin/ppc.out
+gcc -std=c11 -O3 bin/main.c -o bin/ppc.out
 
 rm bin/main.c
-rm bin/preprocessor.c
-rm bin/lexer.c
-rm bin/parser.c
