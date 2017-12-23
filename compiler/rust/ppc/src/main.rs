e4b3778c1c711217df02285a5ab73d293d57fe51
TropicSapling <tropical.productions.scratch@gmail.com>
2017-12-24 00:00:14 +0100

    I/O

--- compiler/rust/ppc/src/main.rs
+++ compiler/rust/ppc/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-	println!("WIP");
+    let args: Vec<String> = env::args().collect();
+	
+	if args.len() < 2 {
+		println!("Please specify an input file.");
+	} else {
+		let input = &args[1];
+		let mut output = &String::from("default");
+		if args.len() > 2 {
+			output = &args[2];
+		}
+
+		println!("In: {}", input);
+		println!("Out: {}", output);
+	}
 }
