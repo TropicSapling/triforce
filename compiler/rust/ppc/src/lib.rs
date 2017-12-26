1e8303fee0a4489a58f6f8719ed357d9e3304a99
TropicSapling <tropical.productions.scratch@gmail.com>
2017-12-26 22:27:13 +0100

    Bugfixes & improvements

--- compiler/rust/ppc/src/lib.rs
+++ compiler/rust/ppc/src/lib.rs
@@ -5,10 +5,14 @@ pub struct Token {
 	t: &'static str
 }
 
+fn is_var(c: char) -> bool {
+	c == '_' || c == '$' || c.is_alphanumeric()
+}
+
 pub fn lex<'a>(contents: &'a String) -> Vec<&'a str> {
 	let mut result = Vec::new();
 	let mut last = 0;
-	for (index, matched) in contents.match_indices(|c: char| !(c.is_alphanumeric())) {
+	for (index, matched) in contents.match_indices(|c: char| !is_var(c)) {
 		if last != index {
 			result.push(&contents[last..index]);
 		}
@@ -33,9 +37,12 @@ pub fn lex2(tokens: Vec<&str>) -> Vec<Token> {
 	};
 	let mut in_str = false;
 	let mut in_str2 = false;
+	let mut escaping = false;
 	
 	for item in tokens {
-		if in_str {
+		if escaping {
+			escaping = false;
+		} else if in_str {
 			if item == "\"" {
 				res.push(string.clone());
 				string.val = String::from("");
@@ -57,14 +64,21 @@ pub fn lex2(tokens: Vec<&str>) -> Vec<Token> {
 		} else if item == "'" {
 			string.t = "str2";
 			in_str2 = true;
+		} else if item == "\\" {
+			escaping = true;
 		} else {
 			string.val = item.to_string();
 			string.t = if item.parse::<u64>().is_ok() {
-				"num"
-			} else if item == "+" || item == "-" || item == "*" || item == "/" || item == "=" || item == "&" || item == "|" || item == "^" || item == ">" || item == "<" || item == "[" || item == "]" || item == "(" || item == ")" || item == "<" || item == ">" {
-				"operator"
+				"number"
 			} else {
-				"var"
+				match item {
+					"+" | "-" | "*" | "/" | "%" | "=" | "&" | "|" | "^" | "<" | ">" | "[" | "]" | "(" | ")" | "!" | "~" | "?" | ":" | "," | "@" | ";" | "{" | "}" => "operator",
+					"array" | "bool" | "chan" | "char" | "const" | "fraction" | "func" | "heap" | "int" | "list" | "number" | "only" | "pointer" | "register" | "signed" | "stack" | "unique" | "unsigned" | "void" | "volatile" => "type",
+					"as" | "async" | "break" | "continue" | "else" | "foreach" | "from" | "goto" | "if" | "in" | "match" | "receive" | "repeat" | "return" | "select" | "send" | "to" | "type" | "until" | "when" | "while" => "reserved",
+					"false" | "true" => "literal",
+					"\n" | "\r" | "\t" | " " => "whitespace",
+					_ => "variable"
+				}
 			};
 			
 			res.push(string.clone());
