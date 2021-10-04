3d8d727be8fed31b329413fc9cf55bd8dc303590
TropicSapling <tropical.productions.scratch@gmail.com>
2021-10-04 22:09:01 +0200

    ST4 plugin basics working

--- st4/Triforce/triforce.py
+++ st4/Triforce/triforce.py
@@ -3,8 +3,6 @@ import sublime_plugin
 
 import re
 
-from threading import Timer
-
 funcs = set()
 ready = True
 
@@ -15,7 +13,10 @@ prelude = {"if", "then", "else", "unless", "for each", "while", "break", "contin
            "could be", "export", "import", "export all", "import all", "except",
            "from", "into", "expose", "with", "all", "in", "excl", "any",
            "any suitable", "any of", "optionally", "recollected", "listified",
-           "codified", "stringified", "ensure", "print", "println"}
+           "codified", "stringified", "ensure", "print", "println", "macro"}
+
+def between(view, a, b):
+	return view.substr(sublime.Region(a, b))
 
 def pythonLambdasDontSupportAssignmentApparently():
 	global ready
@@ -23,86 +24,73 @@ def pythonLambdasDontSupportAssignmentApparently():
 	ready = True
 
 class HighlightTriFuncCallCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
+	def find_funcs(self):
+		global funcs
+		
 		view = self.view
+		size = view.size()
 		
-		lines = view.lines(sublime.Region(0, self.view.size()))
-		for view_line in lines:
-			line   = view.substr(view_line)
-			offset = view_line.to_tuple()[0]
+		i = 0
+		while i < size:
+			while "comment" in view.scope_name(i): i += 1
 			
-			s = ""
-			
-			i = 0
-			while i+5 <= len(line):
-				if line[i:i+2] == "//": break
-				
-				if line[i:i+5] == "func ":
-					a = offset + i
-					b = a
-					while b - offset < len(line):
-						a = b
-						while "entity.name.function.triforce" not in view.scope_name(a):
-							a += 1
-							if a - offset >= len(line): break
-						
-						if a - offset >= len(line): break
-						
-						b = a
-						while "entity.name.function.triforce" in view.scope_name(b):
-							b += 1
-							if b - offset >= len(line): break
-						
-						s += view.substr(sublime.Region(a, b)) + " "
-						
-						i = b - offset
-						while i < len(line) - 1:
-							i += 1
+			if "storage.type" in view.scope_name(i) and between(view, i, i+5) == "func ":
+				s = ""
+				while view.substr(i) != '\n' and i < size:
+					while "entity.name.function" not in view.scope_name(i) and i < size:
+						if view.substr(i) == '\n': break
+						i += 1
 					
-					if line[i] != ';' and line[i] != '{':
-						break
-					elif len(s) > 0 and s[:-1] not in prelude:
-						funcs.add(s[:-1])
+					start = i
+					
+					while "entity.name.function" in view.scope_name(i):
+						if view.substr(i) == '\n': break
+						i += 1
+					
+					if i < size: s += between(view, start, i) + " "
 				
-				i += 1
+				s = s.rstrip()
+				if view.substr(i - 1) == ';' or view.substr(i - 1) == '{':
+					if s != "" and s not in prelude: funcs.add(s)
+			
+			i += 1
 		
-		lines = view.lines(sublime.Region(0, self.view.size()))
-		for view_line in lines:
-			line   = view.substr(view_line)
-			offset = view_line.to_tuple()[0]
-			
-			for f in funcs:
-				regex = r"\b" + re.escape(f) + r"\b"
-				fcall_pos = [m.span() for m in re.finditer(regex, line)]
-				
-				i = 0
-				for (a, b) in fcall_pos:
-					scope = view.scope_name(offset + a)
-					
-					ok = ("comment"                       not in scope and
-					      "string"                        not in scope and
-					      "entity.name.function.triforce" not in scope and
-					      "variable"                      not in scope)
-					
-					if ok and view.substr(offset + a - 1) != '\u200b':
-						# Ensure file hasn't changed while doing previous calculations
-						fcall_pos_now = [m.span() for m in re.finditer(regex, view.substr(view_line))]
-						if len(fcall_pos_now) >= i + 1 and (a, b) == fcall_pos_now[i]:
-							view.insert(edit, offset + a, '\u200b')
-							view.insert(edit, offset + b + 1, '\u200b')
-					
-					i += 1
+		# print(funcs) # DEBUG
+	
+	def highlight_calls(self, edit):
+		view = self.view
+		size = view.size()
 		
-		print(funcs)
+		for f in funcs:
+			regex     = r"\b" + re.escape(f) + r"\b"
+			fcall_pos = [m.span() for m in re.finditer(regex, between(view, 0, size))]
+			
+			offset = 0
+			for (a, b) in fcall_pos:
+				scope = view.scope_name(a + offset)
+				if ("comment"                       in scope or
+					"string"                        in scope or
+					"variable"                      in scope or
+					"entity.name.function.triforce" in scope):
+					continue
+				
+				if view.substr(a + offset) != '\u200b':
+					view.insert(edit, a + offset, '\u200b')
+					view.insert(edit, b + offset + 1, '\u200b')
+					size   += 2
+					offset += 2
+	
+	def run(self, edit):
+		self.find_funcs()
+		self.highlight_calls(edit)
 
 class FuncListener(sublime_plugin.EventListener):
-	def on_selection_modified_async(self, view):
+	def on_modified_async(self, view):
 		global ready
 		
 		if ready and view.window().extract_variables()["file_extension"] == "tri":
 			ready = False
 			view.run_command('highlight_tri_func_call')
 			# Don't update more often than every 5 seconds
-			t = Timer(5, pythonLambdasDontSupportAssignmentApparently)
-			t.start()
-			# TODO: use timer to split up things to make it less laggy
+			sublime.set_timeout_async(pythonLambdasDontSupportAssignmentApparently, 5000)
+			# TODO: use timeout to split up things to make it less laggy
