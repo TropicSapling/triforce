a5c3c282d613f82dae71910a3990cff7a5b68d8c
TropicSapling <tropical.productions.scratch@gmail.com>
2021-10-05 23:08:06 +0200

    ST4 plugin: better faster stronger
    Also: added syntax highlighting for 'TODO' in comments to make them easier to find.

--- st4/Triforce/triforce.py
+++ st4/Triforce/triforce.py
@@ -3,7 +3,9 @@ import sublime_plugin
 
 import re
 
-funcs = set()
+funcs      = set()
+funcs_iter = iter(funcs)
+
 ready = True
 
 prelude = {"if", "then", "else", "unless", "for each", "while", "break", "continue",
@@ -18,80 +20,123 @@ prelude = {"if", "then", "else", "unless", "for each", "while", "break", "contin
 def between(view, a, b):
 	return view.substr(sublime.Region(a, b))
 
-def pythonLambdasDontSupportAssignmentApparently():
-	global ready
-	
-	ready = True
-
 class HighlightTriFuncCallCommand(sublime_plugin.TextCommand):
-	def find_funcs(self):
+	def find_funcs(self, edit):
 		global funcs
+		global funcs_iter
 		
 		view = self.view
 		size = view.size()
 		
-		i = 0
-		while i < size:
-			while "comment" in view.scope_name(i): i += 1
+		for sel in view.sel():
+			i   = min(sel.a, sel.b) - 256
+			end = max(sel.a, sel.b) + 256
 			
-			if "storage.type" in view.scope_name(i) and between(view, i, i+5) == "func ":
-				s = ""
-				while view.substr(i) != '\n' and i < size:
-					while "entity.name.function" not in view.scope_name(i) and i < size:
-						if view.substr(i) == '\n': break
-						i += 1
-					
-					start = i
-					
-					while "entity.name.function" in view.scope_name(i):
-						if view.substr(i) == '\n': break
-						i += 1
-					
-					if i < size: s += between(view, start, i) + " "
+			if i   < 0    : i   = 0
+			if end > size : end = size
+		
+			while i < end:
+				while "comment" in view.scope_name(i): i += 1
 				
-				s = s.rstrip()
-				if view.substr(i - 1) == ';' or view.substr(i - 1) == '{':
-					if s != "" and s not in prelude: funcs.add(s)
-			
-			i += 1
+				if view.substr(i) == '\u200b':
+					# Found existing highlight; dehighlight if no longer valid
+					
+					i += 1
+					start = i
+					while i < end and view.substr(i) != '\u200b':
+						i += 1
+					
+					found_match = False
+					for f in funcs:
+						regex = r"\b" + re.escape(f) + r"\b"
+						term  = between(view, start-2, i+2).replace('\u200b', '')
+						
+						if re.search(regex, term):
+							found_match = True
+							break
+					
+					if not found_match:
+						view.erase(edit, sublime.Region(start-1, start))
+						view.erase(edit, sublime.Region(i-1, i))
+						i   -= 2
+						end -= 2
+				
+				if "storage.type" in view.scope_name(i) and between(view, i, i+5) == "func ":
+					# Found function definition; register
+					
+					s = ""
+					while view.substr(i) != '\n' and i < end:
+						while "entity.name.function" not in view.scope_name(i) and i < end:
+							if view.substr(i) == '\n': break
+							i += 1
+						
+						start = i
+						
+						while "entity.name.function" in view.scope_name(i):
+							if view.substr(i) == '\n': break
+							i += 1
+						
+						if i < end: s += between(view, start, i) + " "
+					
+					s = s.rstrip()
+					if view.substr(i - 1) == ';' or view.substr(i - 1) == '{':
+						if s != "" and s not in prelude: funcs.add(s)
+				
+				i += 1
+		
+		funcs_iter = iter(funcs)
 		
 		# print(funcs) # DEBUG
 	
-	def highlight_calls(self, edit):
+	def highlight_calls(self, edit, size):
 		view = self.view
-		size = view.size()
 		
-		for f in funcs:
-			regex     = r"\b" + re.escape(f) + r"\b"
-			fcall_pos = [m.span() for m in re.finditer(regex, between(view, 0, size))]
+		try:
+			f = next(funcs_iter)
+		except StopIteration:
+			self.find_funcs(edit)
+			return
+		
+		# TODO: highlight longest function name first
+		regex     = r"\b" + re.escape(f) + r"\b"
+		fcall_pos = [m.span() for m in re.finditer(regex, between(view, 0, size))]
+		
+		offset = 0
+		for (a, b) in fcall_pos:
+			scope = view.scope_name(a + offset)
+			if ("comment"                       in scope or
+				"string"                        in scope or
+				"variable"                      in scope or
+				"operator"                      in scope or
+				"entity.name.function.triforce" in scope):
+				continue
 			
-			offset = 0
-			for (a, b) in fcall_pos:
-				scope = view.scope_name(a + offset)
-				if ("comment"                       in scope or
-					"string"                        in scope or
-					"variable"                      in scope or
-					"operator"                      in scope or
-					"entity.name.function.triforce" in scope):
-					continue
-				
-				if view.substr(a + offset) != '\u200b':
-					view.insert(edit, a + offset, '\u200b')
-					view.insert(edit, b + offset + 1, '\u200b')
-					size   += 2
-					offset += 2
+			if view.substr(a + offset - 1) != '\u200b':
+				view.insert(edit, a + offset, '\u200b')
+				view.insert(edit, b + offset + 1, '\u200b')
+				# TODO: fix inserted characters messing with backspace & arrow keys
+				size   += 2
+				offset += 2
+		
+		# Trigger on_modified_async(...) to restart
+		# TODO: fix this causing file to always say it's unsaved
+		view.insert(edit, 0, 'Q')
+		view.erase(edit, sublime.Region(0, 1))
 	
 	def run(self, edit):
-		self.find_funcs()
-		self.highlight_calls(edit)
+		global ready
+		
+		self.highlight_calls(edit, self.view.size())
+		ready = True
 
 class FuncListener(sublime_plugin.EventListener):
 	def on_modified_async(self, view):
 		global ready
 		
-		if ready and view.window().extract_variables()["file_extension"] == "tri":
-			ready = False
-			view.run_command('highlight_tri_func_call')
-			# Don't update more often than every 5 seconds
-			sublime.set_timeout_async(pythonLambdasDontSupportAssignmentApparently, 5000)
-			# TODO: use timeout to split up things to make it less laggy
+		# Need 'try' in case attempting to run after file closed
+		try:
+			if ready and view.syntax().scope == 'source.triforce':
+				ready = False
+				# TODO: fix running command messing with Ctrl+Z
+				view.run_command('highlight_tri_func_call')
+		except AttributeError: ()
