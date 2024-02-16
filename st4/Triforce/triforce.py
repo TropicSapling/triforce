8b87a0aa27b30e492ce7f2b7b598631faad504c6
TropicSapling <tropical.productions.scratch@gmail.com>
2024-02-17 00:46:30 +0100

    Custom types syntax highlighting
    Well, an attempt at it anyway. For now, custom types are not highlighted in italics as they should because ST4 doesn't seem to support that.

--- st4/Triforce/triforce.py
+++ st4/Triforce/triforce.py
@@ -11,7 +11,9 @@ Region.__hash__ = lambda self: hash(str(self))
 alltime_highlights = highlights = set()
 
 funcs      = set()
+types      = set()
 funcs_iter = iter(funcs)
+types_iter = iter(types)
 
 ready = True
 
@@ -55,54 +57,65 @@ def locally(view, f):
 		f(view, i, end)
 
 class TriHighlighter(EventListener):
-	def find_funcs(self, view, i, end):
-		global funcs, funcs_iter
+	def find(self, view, i, end):
+		global funcs, types, funcs_iter, types_iter
 
 		while i < end:
 			while "comment" in view.scope_name(i): i += 1
 
-			if "storage.type" in view.scope_name(i) and between(view, i, i+5) == "func ":
-				# Found function definition; register
+			if "storage.type" in view.scope_name(i):
+				isfunc = between(view, i, i+5) == "func "
+				islet  = between(view, i, i+4) == "let "
 
-				s = ""
-				while view.substr(i) != '\n' and i < end:
-					while "entity.name.function" not in view.scope_name(i) and i < end:
-						if view.substr(i) == '\n': break
-						i += 1
+				if isfunc or islet:
+					# Found func/let definition; register
 
-					start = i
+					s = ""
+					while view.substr(i) != '\n' and i < end:
+						while "entity.name.function" not in view.scope_name(i) and i < end:
+							if view.substr(i) == '\n': break
+							i += 1
 
-					while "entity.name.function" in view.scope_name(i):
-						if view.substr(i) == '\n': break
-						i += 1
+						start = i
 
-					if i < end: s += between(view, start, i) + " "
+						while "entity.name.function" in view.scope_name(i):
+							if view.substr(i) == '\n': break
+							i += 1
 
-				s = s.rstrip()
-				if view.substr(i - 1) == ';' or view.substr(i - 1) == '{':
-					if s != "" and s[0].islower() and re.match(prelude, s) == None:
-						funcs.add(s)
+						if i < end: s += between(view, start, i) + " "
+
+						if islet and between(view, i, i+8) == ": type =": break
+
+					s = s.rstrip()
+					if isfunc and (view.substr(i-1) == ';' or view.substr(i-1) == '{'):
+						if s != "" and s[0].islower() and re.match(prelude, s) == None:
+							funcs.add(s)
+					if islet and between(view, i, i+8) == ": type =":
+						types.add(s)
 
 			i += 1
 
 		funcs_iter = iter(funcs)
+		types_iter = iter(types)
 
-		# print(funcs) # DEBUG
+		# print("funcs:", funcs) # DEBUG
+		# print("types:", types) # DEBUG
 
-	def find_local_funcs(self, view):
-		locally(view, self.find_funcs)
+	def find_local(self, view):
+		locally(view, self.find)
 
-	def find_all_funcs(self, view):
-		self.find_funcs(view, 0, view.size())
+	# Currently unused
+	def find_all(self, view):
+		self.find(view, 0, view.size())
 
-	def highlight_calls(self, start_time, view, start, end):
+	def highlight(self, start_time, iscall, view, start, end):
 		global highlights, alltime_highlights
 
 		try:
-			f = next(funcs_iter)
+			f = next(funcs_iter) if iscall else next(types_iter)
 		except StopIteration:
-			self.dehighlight_calls(view, start, end)
-			self.find_local_funcs(view)
+			self.dehighlight(view, start, end)
+			self.find_local(view)
 			return
 
 		regex     = r"\b" + re.escape(f) + r"\b"
@@ -110,7 +123,7 @@ class TriHighlighter(EventListener):
 
 		for (a, b) in fcall_pos:
 			scopes = view.scope_name(a)
-			scope  = "variable.function.triforce"
+			scope  = "variable.function.triforce" if iscall else "variable.type.triforce"
 
 			if "comment" in scopes or "string" in scopes:
 				if "meta.cc" in scopes and "string" not in scopes:
@@ -137,7 +150,7 @@ class TriHighlighter(EventListener):
 
 		# Keep going for up to 50 ms.
 		if time() - start_time < 0.05:
-			self.highlight_calls(start_time, view, start, end)
+			self.highlight(start_time, iscall, view, start, end)
 
 		# Trigger on_modified_async(...) to restart
 		# TODO: fix this causing file to always say it's unsaved
@@ -145,13 +158,15 @@ class TriHighlighter(EventListener):
 		# view.insert(edit, 0, 'Q')
 		# view.erase(edit, Region(0, 1))
 
-	def highlight_local_calls(self, view):
-		locally(view, partial(self.highlight_calls, time()))
+	def highlight_local(self, view):
+		locally(view, partial(self.highlight, time(), True))
+		locally(view, partial(self.highlight, time(), False))
 
-	def highlight_all_calls(self, view):
-		self.highlight_calls(time(), view, 0, view.size())
+	# Currently unused
+	def highlight_all(self, view):
+		self.highlight(time(), True, view, 0, view.size())
 
-	def dehighlight_calls(self, view, start, end):
+	def dehighlight(self, view, start, end):
 		global highlights
 
 		for highlight in alltime_highlights:
@@ -168,6 +183,6 @@ class TriHighlighter(EventListener):
 		try:
 			if ready and view.syntax().scope == 'source.triforce':
 				setReady(False)
-				self.highlight_local_calls(view)
+				self.highlight_local(view)
 				setReady(True)
 		except AttributeError: pass
