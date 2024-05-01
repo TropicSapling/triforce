7c98d779702e0b01644c18a67e0a414f123139a3
TropicSapling <tropical.productions.scratch@gmail.com>
2024-05-02 00:01:10 +0200

    New compiler beginnings

--- compiler/2024-EDITION/tric/src/main.rs
+++ compiler/2024-EDITION/tric/src/main.rs
@@ -1,3 +1,11 @@
+use std::{fs, io::Error};
+use culpa::throws;
+
+#[throws]
 fn main() {
-    todo!("compiler");
+    let text = fs::read_to_string("../postcard.tri")?;
+
+    println!("");
+
+    dbg!(text);
 }
