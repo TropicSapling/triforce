1af8d88dfcd194ff6cb2c785a60103aa986f4b27
TropicSapling <tropical.productions.scratch@gmail.com>
2024-05-09 22:52:50 +0200

    Lexer rewrite working

--- compiler/2024-EDITION/tric/src/lexer.rs
+++ compiler/2024-EDITION/tric/src/lexer.rs
@@ -1,5 +1,4 @@
-use std::{collections::HashMap, iter::Peekable, str::Chars};
-//use crate::helpers::Substr;
+use std::{collections::HashMap, str::Chars};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
@@ -17,12 +16,14 @@ enum Group {
 	Default,
 	NewlineWs,
 	Whitespace,
-	Custom(HashMap<String, Token>)
+	Custom(HashMap<String, Token>),
+	BegList,
+	EndList
 }
 
 struct Cursor<'a> {
 	group: Group,
-	posit: Peekable<Chars<'a>>,
+	posit: Chars<'a>,
 	token: Token
 }
 
@@ -31,6 +32,8 @@ impl Cursor<'_> {
 		let c = self.skip_if_comment(c);
 
 		let new_group = groups.iter().find(|g| match g {
+			Group::BegList     => c == '(',
+			Group::EndList     => c == ')',
 			Group::Custom(map) => map.keys().any(|s| s.starts_with(c)),
 			Group::NewlineWs   => {
 				c == '\n' || (c.is_whitespace() && self.group == Group::NewlineWs)
@@ -39,7 +42,9 @@ impl Cursor<'_> {
 			Group::Default     => true
 		}).unwrap();
 
-		if self.group == *new_group {
+		if self.group != Group::BegList
+		&& self.group != Group::EndList
+		&& self.group == *new_group {
 			// Extend current token
 			match &mut self.token {
 				Token::Default(ref mut tokstr) |
@@ -54,6 +59,8 @@ impl Cursor<'_> {
 
 			self.group = new_group.clone();
 			match new_group {
+				Group::BegList    => self.token = self.beglist(),
+				Group::EndList    => self.token = Token::EndList,
 				Group::NewlineWs  => self.token = Token::Newline,
 				Group::Whitespace => self.token = Token::Ignored,
 				Group::Default    => self.token = Token::Default(c.to_string()),
@@ -65,10 +72,19 @@ impl Cursor<'_> {
 	}
 
 	fn skip_if_comment(&mut self, c: char) -> char {
-		if c == '/' && self.posit.peek().is_some_and(|c| *c == '/') {
+		if c == '/' && self.posit.clone().next().is_some_and(|c| c == '/') {
 			self.posit.find(|c| *c == '\n'); '\n'
 		} else {
 			c
+		}
+	}
+
+	fn beglist(&mut self) -> Token {
+		if self.posit.as_str().starts_with(">>>") {
+			self.posit.nth(2);
+			Token::BegOpenList
+		} else {
+			Token::BegList
 		}
 	}
 }
@@ -76,12 +92,9 @@ impl Cursor<'_> {
 // Tokenises based on "maximal munch"
 pub fn tokenised(code: String) -> Vec<Token> {
 	let mut tokens: Vec<Token> = vec![];
-	let     groups: Vec<Group> = vec![
-		Group::Custom(HashMap::from([
-			(String::from("(>>>"), Token::BegOpenList),
-			(String::from("("   ), Token::BegList    ),
-			(String::from(")"   ), Token::EndList    )
-		])),
+	let mut groups: Vec<Group> = vec![
+		Group::BegList,
+		Group::EndList,
 		Group::NewlineWs,
 		Group::Whitespace,
 		Group::Default
@@ -89,7 +102,7 @@ pub fn tokenised(code: String) -> Vec<Token> {
 
 	let mut cursor = Cursor {
 		group: Group::Whitespace,
-		posit: code.chars().peekable(),
+		posit: code.chars(),
 		token: Token::Ignored
 	};
 
@@ -102,113 +115,3 @@ pub fn tokenised(code: String) -> Vec<Token> {
 
 	tokens
 }
-/*
-// Tokenises based on "maximal munch"
-pub fn tokenised(code: String) -> Vec<Token> {
-	let mut tokens: Vec<Token> = vec![];
-
-	let standalones = HashMap::from([
-		("(>>>" , Token::BegOpenList),
-		("("    , Token::BegList    ),
-		(")"    , Token::EndList    )
-	]);
-
-	/*let groups = vec![
-		Group::Default,
-		Group::NewlineWs,
-		Group::Whitespace
-	];*/
-
-	let mut superpos: Vec<String> = vec![];
-	let mut superend = (usize::MAX, usize::MAX);
-
-	let mut comment  = false;
-	let mut group    = Group::Default;
-	let mut it       = code.chars().peekable();
-	let mut nxt_it   = it.clone();
-	while let Some(c) = it.next() {
-		if comment && c != '\n' {continue}
-
-		comment = false;
-		match group {
-			Group::Default => if c == '\n' || c.is_whitespace() {
-				if let Some(c) = it.peek() {
-					if c.is_whitespace() {
-						group = if *c == '\n' {Group::NewlineWs} else {Group::Whitespace}
-					}
-				}
-
-				if superpos.len() > 0 {
-					if superend.0 == usize::MAX {
-						tokens.push(Token::Default(superpos[0].clone()))
-					} else {
-						if superend.0 > 0 {
-							tokens.push(Token::Default(superpos[0].substr(..superend.0)))
-						}
-
-						tokens.push(standalones.get(superpos[superend.0].substr(..superend.1).as_str()).unwrap().clone());
-						it = nxt_it.clone();
-						group = Group::Default
-					}
-				}
-
-				if c == '\n' {tokens.push(Token::Newline)}
-
-				superpos = vec![];
-				superend = (usize::MAX, usize::MAX)
-			} else {
-				if superend.0 == usize::MAX {
-					superpos.push(String::new())
-				} else {
-					superpos.truncate(superend.0 + 1)
-				}
-
-				for (i, alt) in superpos.iter_mut().enumerate() {
-					alt.push(c);
-					if standalones.get(alt.as_str()).is_some() {
-						superend = (i, alt.len());
-						nxt_it   = it.clone()
-					} else if alt == "//" {
-						comment = true
-					}
-				}
-
-				if comment {
-					superpos = vec![];
-					superend = (usize::MAX, usize::MAX)
-				}
-			}
-
-			Group::NewlineWs => {
-				if tokens[tokens.len() - 1] != Token::Newline {
-					tokens.push(Token::Newline)
-				}
-
-				if let Some(c) = it.peek() {
-					if !c.is_whitespace() {
-						group = Group::Default
-					}
-				}
-			}
-
-			Group::Whitespace => {
-				if c == '\n' {
-					group = Group::NewlineWs;
-					if tokens[tokens.len() - 1] != Token::Newline {
-						tokens.push(Token::Newline)
-					}
-				}
-
-				if let Some(c) = it.peek() {
-					if !c.is_whitespace() {
-						group = Group::Default
-					}
-				}
-			}
-
-			_ => todo!()
-		}
-	}
-
-	tokens
-}*/
