10bca9c125cf0b5408bd8827ef1f3ed0940273a7
TropicSapling <tropical.productions.scratch@gmail.com>
2024-05-16 00:40:01 +0200

    Parser working

--- compiler/2024-EDITION/tric/src/parser/parser.rs
+++ compiler/2024-EDITION/tric/src/parser/parser.rs
@@ -6,10 +6,48 @@ pub enum Expr {
 	Atom(Token)
 }
 
+fn parsed_list(posit: &mut impl Iterator<Item = Token>) -> Expr {
+	let mut list = vec![];
+
+	let mut param = false;
+	while let Some(token) = posit.next() {
+		match token {
+			Token::Default(tokstr) |
+			Token::Default(tokstr) if tokstr == "Λ" || tokstr == "λ" => param = true,
+
+			Token::BegOpenList => {
+				list.push(parsed_list(posit));
+				if param {
+					param = false
+				} else {
+					break
+				}
+			}
+
+			Token::BegList => {
+				list.push(parsed_list(posit));
+				param = false
+			}
+
+			Token::EndList => {
+				param = false;
+				break
+			}
+
+			_ => {
+				list.push(Expr::Atom(token));
+				param = false
+			}
+		}
+	}
+
+	Expr::List(list)
+}
+
 pub fn parsed(tokens: Vec<Token>) -> Expr {
-	use Expr::*;
-
-	// TODO ...
-
-	List(vec![Atom(tokens[0].clone())]) // tmp placeholder
+	if tokens.len() > 1 {
+		parsed_list(&mut tokens.into_iter())
+	} else {
+		Expr::List(vec![Expr::Atom(tokens[0].clone())])
+	}
 }
